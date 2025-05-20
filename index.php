8232e020ca27703368bf2c0c9c8529b0e3c005c3
GenesisAdduru <202310334@fit.edu.ph>
2025-05-20 14:39:37 +0800

    refactor(landing page): Removed unnnecessary codes

--- index.php
+++ index.php
@@ -4,20 +4,6 @@ require BASE_PATH . '/utils/htmlEscape.utils.php';
 
 require_once BASE_PATH . '/handlers/pageData.handler.php';
 require_once BASE_PATH . '/components/cards/note.component.php';
-
 require_once BASE_PATH . '/components/templates/head.component.php';
 require_once BASE_PATH . '/components/templates/nav.component.php';
-?>
-
-<?php
-head('Product List');
-navHeader($navList);
-?>
-
-<section class="grid grid-cols-3">
-    <?php displayCardForm($productList); ?>
-</section>
-
-<?php
-footer();
 ?>
