ca2d36a28d4c9b53fd52832cf3ee7fd3868be7cd
GenesisAdduru <202310334@fit.edu.ph>
2025-05-20 14:37:18 +0800

    refactor(landing page): moved the head structure to components

--- components/templates/head.component.php
+++ components/templates/head.component.php
@@ -1,19 +1,9 @@
-<?php
-function head($pageTitle)
-{
-    ?>
-    <!DOCTYPE html>
-    <html lang="en">
-
-        <head>
-            <meta charset="utf-8">
-            <title><?= htmlEscape($pageTitle ?? 'My Shop') ?></title>
-
-            <!-- Libraries -->
-            <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-        </head>
-
-        <body>
-            <?php
-}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Bossnobenta</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
