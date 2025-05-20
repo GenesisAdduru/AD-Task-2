a24070492913861523d3a44c54f1a2a5b9edb706
GenesisAdduru <202310334@fit.edu.ph>
2025-05-20 15:13:29 +0800

    feat(landing page): Added body style

--- components/templates/nav.component.php
+++ components/templates/nav.component.php
@@ -1,7 +1,7 @@
 <nav>
   <div class="logo"><a href="index.php">Bossnobenta</a></div>
   <ul>
-    <li><a href="bikes.php">BIKES</a></li>
-    <li><a href="apparel.php">APPAREL</a></li>
+    <li><a href="../../pages/Bikes/index.php">BIKES</a></li>
+    <li><a href="../../pages/Apparel/index.php">APPAREL</a></li>
   </ul>
 </nav>
--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 require BASE_PATH . '/vendor/autoload.php';
-require BASE_PATH . '/utils/htmlEscape.utils.php';
 
-require_once BASE_PATH . '/handlers/pageData.handler.php';
-require_once BASE_PATH . '/components/cards/note.component.php';
 require_once BASE_PATH . '/components/templates/head.component.php';
 require_once BASE_PATH . '/components/templates/nav.component.php';
 ?>
+  <header class="hero">
+    <img src="assets/img/bossnobenta.jpg" alt="Hero" class="hero-img" />
+    <a href="bikes.php" class="shop-now-btn">SHOP NOW</a>
+  </header>
+</body>
+</html>
