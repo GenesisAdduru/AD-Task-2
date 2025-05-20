0c8c568def1f5a95acce58283c9b301f77a2704f
GenesisAdduru <202310334@fit.edu.ph>
2025-05-20 19:18:58 +0800

    fix (landing page): fixed shop now button

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once BASE_PATH . '/components/templates/nav.component.php';
 ?>
   <header class="hero">
     <img src="assets/img/bossnobenta.jpg" alt="Hero" class="hero-img" />
-    <a href="bikes.php" class="shop-now-btn">SHOP NOW</a>
+    <a href="/pages/Bikes/index.php" class="shop-now-btn">SHOP NOW</a>
   </header>
 </body>
 </html>
